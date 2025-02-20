e1920e9bbc49026fadb5d0ec52d71eeaf3174b78
javohir-abdunabiyev <jahaajahaaa2@gmail.com>
2025-02-20 22:23:08 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {FeaderFunc, DeskSystemSection, DevicesSection, ProductsFunc, OfficeInspiration} from './components/functions'
+import './App.css'
+
+
+const Devices = [
+  {
+      img: "/imgs/iMacs.png",
+      name: "Desk Pads",
+      more: "LEARN MORE"
+  },
+  {
+      img: "/imgs/macbooks.png",
+      name: "Laptop Stands",
+      more: "LEARN MORE"
+  }
+]
+
+const products = [
+  {
+    img: "/imgs/iphone.png",
+    name: "Walnut Laptop Riser",
+    cost: "$" + 120,
+  },
+  {
+    img: "/imgs/laptop.png",
+    name: "Walnut MagSafe Stand",
+    cost: "$" + 150,
+  },
+  {
+    img: "/imgs/ipad.png",
+    name: "Walnut iPad Stand",
+    cost: "$" + 80,
+  },
+  {
+    img: "/imgs/monitor.png",
+    name: "Walnut Monitor Stand",
+    cost: "$" + 100,
+  }
+]
+
+
+function App() {
+
+  return (
+    <center>
+      <div className='container'>
+        <FeaderFunc />
+      </div>
+
+      <DeskSystemSection/>
+
+      <div className="container">
+        <div className="designInspires">
+          <h1>Design Inspires</h1>
+          <p className='txt'>Build your dream workspace, so you can get your best work done.</p>
+          <p className='getStarted'>GET STARTED</p>
+        </div>
+
+        <div className="devicesDiv">
+          {
+            Devices.map((device, idx) => (<DevicesSection key={idx} device={device} />))
+          }
+        </div>
+
+        <div className="designInspires">
+          <h1>Featured Products</h1>
+          <p className='txt'>See What’s Trending Right Now</p>
+        </div>
+
+        <div className="productsDiv">
+          {
+            products.map((product, idx) => (<ProductsFunc key={idx} product={product} />))
+          }
+        </div>
+
+      </div>
+
+      <OfficeInspiration/>
+
+    </center>
+  )
+}
+
+export default App
---
+++ src/components/functions.jsx
@@ -0,0 +1,77 @@
+function FeaderFunc() {
+    return (
+        <div className="header">
+            <nav className="headerNav">
+
+                <div className="options">
+                    <span>SHOP</span>
+                    <span>EXPLORE</span>
+                </div>
+
+                <img className="logo" src="/imgs/logo.png" alt="" />
+
+                <div className="cart">MY CART</div>
+
+            </nav>
+        </div>
+    )
+}
+
+
+
+function DeskSystemSection() {
+    return (
+        <div className="deskSystem">
+
+            <img className="deskImg" src="/imgs/desk.png" alt="" />
+            
+            <div className="description">
+                <h1>The Desk Shelf System</h1>
+                <p>Available in Walnut or Maple</p>
+                <p>LEARN MORE</p>
+            </div>
+
+        </div>
+    )
+}
+
+
+function DevicesSection({device}) {
+    return (
+        <div className="devicesBlock">
+            <img className="deviceImg" src={device.img} alt="" />
+            <p className="deviceName">{device.name}</p>
+            <p className="more">{device.more}</p>
+        </div>
+    )
+}
+
+
+function ProductsFunc({product}) {
+    return (
+        <div className="productBlock">
+            <img className="productsImg" src={product.img} alt="" />
+            <p className="productName">{product.name}</p>
+            <p className="productCost">{product.cost}</p>
+        </div>
+    )
+}
+
+function OfficeInspiration() {
+    return (
+        <div className="deskSystem officeSystem">
+
+            <img className="deskImg" src="/imgs/office.png" alt="" />
+            
+            <div className="description">
+                <h1>Home Office Inspiration</h1>
+                <p>Working from home can be a challenge—see some creative solutions to get it just right.</p>
+                <p>LEARN MORE</p>
+            </div>
+
+        </div>
+    )
+}
+
+
+export {FeaderFunc, DeskSystemSection, DevicesSection, ProductsFunc, OfficeInspiration};
